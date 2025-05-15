656d1ecf35f984d4011434c3094fe81d9587f561
mugesh1817-s <140521078+mugesh1817-s@users.noreply.github.com>
2025-05-15 11:08:08 +0530

    Delete attendance directory

--- attendance/myapp/management/commands/show_attendance.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from myapp.models import Attendance
-
-class Command(BaseCommand):
-    help = 'Display current attendance data'
-
-    def handle(self, *args, **kwargs):
-        for att in Attendance.objects.all().order_by('-timestamp')[:20]:
-            self.stdout.write(f'{att.employee.name} - {att.timestamp} - {att.status}')
--- attendance/myapp/utils.py
+++
@@ -1,6 +0,0 @@
-
-def sync_attendance():
-    # Your sync logic goes here
-    print("✅ Attendance sync logic executed!")
-
-    # Example: Save to DB, clean data, fetch from device
